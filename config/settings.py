a2a16af04d6e58332acda749b1d5e384333155e1
Viktor X <x.viktor.13@gmail.com>
2022-04-27 15:16:49 +0700

    Add required libraries

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
